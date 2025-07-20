47510ad04022bb5705ee78a105d5021ad0b33089
souravdutta06 <souravdutta06@gmail.com>
2025-07-20 06:59:17 +0530

    Add files via upload

---
+++ HelloWorldWebApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HelloWorldWebApp.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
